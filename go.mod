919b94533d0b2afd52b46b229f35ef41b7c905f2
Daniel Kravetz Malabud <daniel.kravetz@gmail.com>
2020-10-30 23:59:33 +0100

    :construction: WIP: Add Fuzzy Search based on item name

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/dkravetz/risk_of_rain_items_chatbot
 
 go 1.14
 
-require github.com/PuerkitoBio/goquery v1.6.0
+require (
+	github.com/PuerkitoBio/goquery v1.6.0
+	github.com/lithammer/fuzzysearch v1.1.1
+	github.com/sahilm/fuzzy v0.1.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/dkravetz/risk_of_rain_items_chatbot/internal/ror2"
+	"github.com/sahilm/fuzzy"
 )
 
 func main() {
+	wordPtr := flag.String("word", "Tri-Tip Dagger", "Name of item to search")
+	flag.Parse()
+
 	items, err := ror2.GetAllItemsFromFile("files/ror2_items.html")
+	// items, err := ror2.GetAllItemsFromURL("https://riskofrain2.gamepedia.com/Items#Boss")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(items[83])
+
+	results := fuzzy.FindFrom(*wordPtr, items)
+
+	fmt.Println(items[results[0].Index])
 
 }
